9447afa1e71b97e249771658ba0739d9e796ad0a
amandameneseso <amandamenezes594@gmail.com>
2025-02-04 11:29:48 -0300

    feat: starts project with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-red-600">
+        Minha Loja de Roupas
+      </h1>
+    </div>
+  );
+};
+
+export default App;
