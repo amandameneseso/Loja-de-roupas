b9dfc46208a22519514b510e9c5f0689ed90d23e
amandameneseso <amandamenezes594@gmail.com>
2025-07-11 18:19:31 -0300

    shopcontext

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 // Home.jsx
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
 
 const Home = () => {
-    return (
-        <div>
-            <Hero />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
