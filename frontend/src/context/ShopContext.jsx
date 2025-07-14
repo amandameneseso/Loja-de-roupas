30f56eb5a22c110400aafb081a6744e3962beef8
Ranilo <amandamenezes594@gmail.com>
2025-07-14 11:04:40 -0300

    search bar

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 // ShopContext.jsx
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets.js";
 
 export const ShopContext = createContext();
@@ -7,11 +7,17 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "R$ ";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
   const value = {
     products,
     currency,
     delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return (
