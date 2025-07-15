5e3acdc0dfdad7938169bc1be156294e77dcde53
amandameneseso <amandamenezes594@gmail.com>
2025-07-15 12:42:35 -0300

    função getcartcount

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -33,9 +33,21 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   }
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getCartCount = () => {
+    let totalCount = 0;
+    for(const items in cartItems) {
+      for(const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    return totalCount;
+  }
 
   const value = {
     products,
@@ -46,7 +58,8 @@ const ShopContextProvider = (props) => {
     showSearch,
     setShowSearch,
     cartItems,
-    addToCart
+    addToCart,
+    getCartCount
   };
 
   return (
