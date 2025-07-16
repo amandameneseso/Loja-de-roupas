c6e7e1365e81fc17b0cd22006e4cc4c9a6267f5a
Ranilo <amandamenezes594@gmail.com>
2025-07-16 20:27:05 -0300

    pagina de pagamento

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import { assets } from "../assets/frontend_assets/assets.js";
 import CartTotal from "../components/CartTotal.jsx";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
   useEffect(() => {
@@ -66,9 +66,12 @@ const Cart = () => {
       <div className='flex justify-end my-20'>
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
+          <div className='w-full text-end'>
+            <button onClick={()=>navigate("/place-order")} className='bg-black text-white text-sm my-8 px-8 py-3'>IR PARA O PAGAMENTO</button>
+          </div>
         </div>
       </div>
-      
+
     </div>
   );
 };
