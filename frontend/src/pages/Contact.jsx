0dabca767a5acb33f6a339a41c1853664e6aa8d1
Ranilo <amandamenezes594@gmail.com>
2025-07-18 11:14:27 -0300

    pagina de contato

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,12 +1,32 @@
 // Contact.jsx
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets.js";
+import NewsletterBox from "../components/NewsletterBox.jsx";
 
 const Contact = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"ENTRE EM"} text2={"CONTATO"} />
+      </div>
 
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Nossa loja</p>
+          <p className='text-gray-500'>Rua azul, 123</p>
+          <p className='font-semibold text-xl text-gray-600'>Atendimento</p>
+          <p className='text-gray-500'>Telefone: (xx) xxxxx-xxxx <br /> E-mail: contato@lojamaro.com</p>
+          <p></p>
+          <p></p>
+          <p></p>
         </div>
-    )
-}
+      </div>
 
-export default Contact
+      <NewsletterBox />
+    </div>
+  );
+};
+
+export default Contact;
