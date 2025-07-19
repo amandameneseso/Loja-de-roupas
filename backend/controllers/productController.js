0ec85c1e82375b2f85495bb81489633b83a40254
amandameneseso <amandamenezes594@gmail.com>
2025-07-19 13:06:11 -0300

    iniciando gerenciamento de produtos

---
+++ backend/controllers/productController.js
@@ -0,0 +1,15 @@
+// controllers/productController.js
+
+// função para adicionar um produto ao banco de dados
+const addProduct = async (req, res) => {}
+
+// função para listar os produtos do banco de dados
+const listProducts = async (req, res) => {}
+
+// função para remover um produto do banco de dados
+const removeProduct = async (req, res) => {}
+
+// função para obter um produto do banco de dados
+const singleProduct = async (req, res) => {}
+
+export { addProduct, listProducts, removeProduct, singleProduct };
---
+++ backend/middleware/multer.js
@@ -0,0 +1,15 @@
+// middleware/multer.js
+import multer from "multer";
+
+// configurações de armazenamento do multer
+const storage = multer.diskStorage({
+  // define o nome do arquivo no destino
+  filename: function (req, file, callback) {
+    callback(null, file.originalname); // usa o nome original do arquivo
+  },
+});
+
+// inicializa o multer
+const upload = multer({ storage });
+
+export default upload;
