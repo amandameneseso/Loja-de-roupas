5b0267d19ef17fecd71f5669f3a0fe2b87b484e8
amandameneseso <amandamenezes594@gmail.com>
2025-07-19 10:04:24 -0300

    backend rotas e userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+// rota para login do usuário
+const loginUser = async (req, res) => {};
+
+// rota para cadastro do usuário
+const registerUser = async (req, res) => {};
+
+// rota para login do admin
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
