138921418fe36048228554affef061eef7660962
Ranilo <amandamenezes594@gmail.com>
2025-07-24 10:07:02 -0300

    menu do usuario

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,20 +44,21 @@ const Navbar = () => {
         <div className="flex items-center gap-6">
           <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
           <div className="group relative">
-            <Link to="/login">
-              <img
+            <img
+                onClick={() => token ? null : navigate('/login')}
                 src={assets.profile_icon}
                 className="w-5 cursor-pointer"
                 alt=""
-              />
-            </Link>
+            />
+            {/* Menu do usuário*/}
+            {token &&
             <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
               <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rouded">
                 <p className="cursor-pointer hover:text-black">Meu perfil</p>
                 <p className="cursor-pointer hover:text-black">Pedidos</p>
                 <p onClick={logout} className="cursor-pointer hover:text-black">Sair</p>
               </div>
-            </div>
+            </div>}
           </div>
           <Link to="/cart" className="relative">
             <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
