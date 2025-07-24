54f64ab0fd47cfbd4d8517d2d1d70a8b60976f96
Ranilo <amandamenezes594@gmail.com>
2025-07-24 11:01:28 -0300

    cart backend

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+//adicionar produtos ao carrinho
+const addToCart = async (req, res) => {
+
+}
+
+//atualizar carrinho
+const updateCart = async (req, res) => {
+    
+}
+
+//obter dados do carrinho
+const getUserCart = async (req, res) => {
+    
+}
+
+export { addToCart, updateCart, getUserCart }
