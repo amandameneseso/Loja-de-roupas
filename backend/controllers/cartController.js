1768e638f24ace2c0a29d06d62652a2a729223c1
Ranilo <amandamenezes594@gmail.com>
2025-07-24 12:07:53 -0300

    cartController

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,6 +1,14 @@
+import userModel from "../models/userModel.js";
+
 //adicionar produtos ao carrinho
 const addToCart = async (req, res) => {
-
+    try {
+        const { userId, itemId, size } = req.body;
+        const userData = await userModel.findById(userId);
+        const cartData = await userData.cartData;
+    } catch (error) {
+        
+    }
 }
 
 //atualizar carrinho
